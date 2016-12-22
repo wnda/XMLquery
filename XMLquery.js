f2e840f24452185bdbf9c3e98917f450264e1c2a
A. M. Douglas <me@amdouglas.com>
2016-12-22 19:12:18 +0000

    Update XMLquery.js

--- XMLquery.js
+++ XMLquery.js
@@ -1,80 +1,123 @@
 ;(function (win, doc) {
 
+  'use strict';
+
+  if (typeof win === 'undefined' || typeof doc === 'undefined') return void 0;
+
+  if (typeof win.console === 'undefined' ) {
+    win.console = {
+      "log": alertLog,
+      "error": alertLog
+    };
+  }
+
   win.XMLquery = XMLquery;
 
   function XMLquery (config) {
-    var xhr;
-    var xml_doc;
-    var parser;
-    var url;
-    var callback;
+    var parser, xml_doc, use_cors, xhr, xdr;
 
     switch (true) {
       case !('DOMParser' in win):
+        win.console.error('Your browser does not support DOMParser');
+        return void 0;
+
       case !('XMLHttpRequest' in win):
+        win.console.error('Your browser does not support basic AJAX');
+        return void 0;
+
       case (typeof config !== 'object'):
-      case !('url' in config):
-      case !('callback' in config):
+        win.console.error('You have not supplied configuration settings');
+        return void 0;
+
       case (typeof config.url !== 'string'):
+        win.console.error('You have not supplied a URL');
+        return void 0;
+
       case (typeof config.callback !== 'function'):
-      case (!config.url || !config.callback):
-        return;
+        win.console.error('You have not specified a callback function');
+        return void 0;
+
+      case !(config.url):
+        win.console.error('Your URL should be a non-empty string');
+        return void 0;
+
+      case !(config.url.match(/^(?:https?):\/\//)):
+        win.console.error('Please supply a HTTP protocol');
+        return void 0;
+
+      default:
+        parser = new win.DOMParser();
+        use_cors = win.location.host !== config.url.match(/^(?:https?):\/\/(?:[^@:\/]*@)?([^:\/]+)/)[1] ? 'cors' : 'no-cors';
     }
 
-    parser = new win.DOMParser();
+    switch (true) {
+      case ('fetch' in win):
+        win.fetch(config.url, {
+          "mode": use_cors
+        }).then(function (resp) {
+          if (resp.ok) {
+            return resp.text().then(function (xml) {
+              xml_doc = parser.parseFromString(xml, 'text/xml');
+              if (typeof xml_doc !== 'object') return;
+              return config.callback(xml_doc);
+            }).catch(function (e) {
+              win.console.warn('Failed to process response; error: ' + e);
+              return void 0;
+            });
+          }
+        }).catch(function (e) {
+          win.console.warn('Failed to fetch; error: ' + e);
+          return void 0;
+        });
+        break;
 
-    if ('fetch' in win) {
-
-      win.fetch(config.url, {
-        'mode': !!config.cors ? 'cors' : 'no-cors'
-      }).then(function (resp) {
-
-        if (resp.ok) {
-          return resp.text().then(function (xml) {
-
-            xml_doc = parser.parseFromString(xml, 'text/xml');
-            if (typeof xml_doc !== 'object') return;
-            return config.callback(xml_doc);
-
-          }).catch(function (e) {
-            win.console.warn('Failed to process response; error: ' + e);
-          });
-        }
-
-      }).catch(function (e) {
-        win.console.warn('Failed to fetch; error: ' + e);
-      });
-
-    } else {
-
-      xhr = new win.XMLHttpRequest();
-      xhr.open('GET', config.url, true);
-
-      if ('responseType' in xhr) xhr.responseType = 'text';
-
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300) {
-
-          xml_doc = parser.parseFromString(xhr.responseText, 'text/xml');
+      case ('XDomainRequest' in win && use_cors === 'cors'):
+        xdr = new win.XDomainRequest();
+        xdr.open('GET', config.url, true);
+        xdr.onload = function () {
+          xml_doc = parser.parseFromString(xdr.responseText, 'text/xml');
           if (typeof xml_doc !== 'object') return;
           return config.callback(xml_doc);
+        };
+        xdr.ontimeout = xdr.onerror = xdr.onabort = function () {
+          xdr.ontimeout = function () {};
+          xdr.onerror = function () {};
+          xdr.onabort = function () {};
+          xdr.onload = function () {};
+          win.console.warn('XDR failed');
+          return void 0;
+        };
+        win.setTimeout(xdr.send(), 0);
+        break;
 
-        }
-      };
-
-      xhr.ontimeout = xhr.onerror = xhr.onabort = function () {
-        xhr.onreadystatechange = function () {};
-        xhr.ontimeout = function () {};
-        xhr.onerror = function () {};
-        xhr.onabort = function () {};
-        win.console.warn('XHR failed. Status code: ' + xhr.status + '; readyState: ' + xhr.readyState);
-      };
-
-      win.setTimeout(
-        xhr.send(null),
-      0);
+      default:
+        xhr = new win.XMLHttpRequest();
+        xhr.open('GET', config.url, true);
+        if ('responseType' in xhr) xhr.responseType = 'text';
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300) {
+            xml_doc = parser.parseFromString(xhr.responseText, 'text/xml');
+            if (typeof xml_doc !== 'object') return;
+            return config.callback(xml_doc);
+          }
+        };
+        xhr.ontimeout = xhr.onerror = xhr.onabort = function () {
+          xhr.ontimeout = function () {};
+          xhr.onerror = function () {};
+          xhr.onabort = function () {};
+          xhr.onreadystatechange = function () {};
+          win.console.warn('XHR failed. Status code: ' + xhr.status + '; readyState: ' + xhr.readyState);
+          return void 0;
+        };
+        win.setTimeout(xhr.send(), 0);
     }
+  }
 
+  function alertLog () {
+    var i = 0;
+    for ( ; i < arguments.length; ++i) {
+      alert(arguments[i]);
+    }
   }
 
 })(window, window.document);
